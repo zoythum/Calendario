c4a07adf6411605b7cc54dbe671dc7869e52e6bb
Alessandro Nazzari <alessandro.nazzari@gmail.com>
2018-11-18 16:19:45 +0100

    Integrazione Facebook
    il calendario si ottiene ora a partire da quello fornito da Facebook selezionando solo i compleanni a cui si è interessati

--- calendario.py
+++ calendario.py
@@ -3,69 +3,67 @@ from datetime import datetime
 import pytz
 
 def save_cal(calendar):
-	output_file = open('calendarioprova.ics', 'wb')
+	output_file = open('Compleanni.ics', 'wb')
 	output_file.write(calendar.to_ical())
 	output_file.close()
 
+def obtain_birthdays():
+	compleanni = {}
+	i = 0
+	k = 0
+	
+	with open('/Users/alessandronazzari/Documents/Progetti/Python/Calendario/complenniprova.txt') as file:
+		content = [line.rstrip('\n') for line in file]	
 
-def month_to_int(month):
-	if (isinstance(month, str)):
-		month = month.lower()
-		if month == 'gennaio':
-			return(1)
-		elif month == 'febbraio':
-			return(2)
-		elif month == 'marzo':
-			return(3)
-		elif month == 'aprile':
-			return(4)
-		elif month == 'maggio':
-			return(5)
-		elif month == 'giugno':
-			return(6)
-		elif month == 'luglio':
-			return(7)
-		elif month == 'agosto':
-			return(8)
-		elif month == 'settembre':
-			return(9)
-		elif month == 'ottobre':
-			return(10)
-		elif month == 'novembre':
-			return(11)
-		elif month == 'dicembre':
-			return(12)									
-	elif ((isinstance(month, int))or(isinstance(month, float))):
-		return(int(month))
+	while i < len(content):
+		if (content[i] == ''):
+			content.pop(i)
+		i+=1
 
+	while k < len(content):
+		summary = content[k].split(':')[1].strip()
+		date = content[k+1].split(':')[1].strip()
+		compleanni[summary] = date
+		k += 2
 
+	return(compleanni)
+
+def populate_calendar(compleanni, calendar):
+
+	timezone = pytz.timezone('Europe/Rome')
+	for keys in compleanni:
+		nome_festeggiato = keys.replace("Compleanno di ", "")
+		to_add = input("vuoi aggiungere il compleanno di {}? (y/n)".format(nome_festeggiato))	
+
+		if (to_add == 'y'):
+			print("Ok, lo aggiungo\n")
+			event = Event()
+			event.add('summary', keys)
+			month, day = convert_date(compleanni[keys])
+			data_inizio = datetime(2019, int(month), int(day), 10, 00, tzinfo=timezone)
+			data_fine = datetime(2019, int(month), int(day), 11, 00, tzinfo=timezone)
+			event.add('dtstart', data_inizio)
+			event.add('dtend', data_fine)
+			calendar.add_component(event)
+		elif (to_add == 'n'):
+			print("Va bene, non sarà aggiunto\n")
+		else:
+			print("Rispondi solo con y/n")
+	return(calendar)
+
+def convert_date(date):
+	lista_data = date.split('-')
+	mese = lista_data[1]
+	giorno = lista_data[2]
+	return(mese, giorno)
 
 def main():
-	print ("Inserisci tutti i compleanni che vuoi salvare")
 
-	add_new = "yes"
+	print("Seleziona quali compleanni vuoi salvare, rispondi solo con y/n")
+	compleanni = obtain_birthdays()
 	cal = Calendar()
-
-	while(add_new != 'no'):
-		festeggiato = input("Di chi è il compleanno?")
-		month = input("in che mese compie gli anni?")
-		month = month_to_int(month)
-		day = int(input("che giorno?"))
-		timezone = pytz.timezone('Europe/Rome')
-		data_inizio = datetime(2019, month, day, 10, 00, tzinfo=timezone)
-		data_fine = datetime(2019, month, day, 11, 00, tzinfo=timezone)
-
-		event = Event()
-		event.add('summary', "compleanno di " + festeggiato)
-		event.add('dtstart', data_inizio)
-		event.add('dtend', data_fine)
-
-		cal.add_component(event)
-		add_new = input("vuoi aggiungere un altro compleanno?")
-
+	cal = populate_calendar(compleanni, cal)
 	save_cal(cal)
-
-
 
 
 
